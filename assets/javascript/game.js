7fb71d6f02c8673e30545443c8d03b79693e1811
Ali Mustafa <ali.taweel7@gmail.com>
2019-10-13 15:02:06 -0400

    added instructions to readme file and start JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// Min/Max 19-120
+let randomMin = 19;
+let randomMax = 120;
+
+// Crystal min max
+let crystalMin = 1;
+let crystalMax = 12;
+
+let wins = 0;
+let losses = 0;
+let crystal1;
+let crystal2;
+let crystal3;
+let crystal4;
+let playerScore = 0;
