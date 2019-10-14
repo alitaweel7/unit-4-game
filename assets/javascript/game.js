f273c3c398080cd409b5ff42f652e5650d533407
Ali Mustafa <ali.taweel7@gmail.com>
2019-10-13 23:28:34 -0400

    updated JS code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ let randomMax = 120;
 // Crystal min max
 let crystalMin = 1;
 let crystalMax = 12;
-
+let random = 0;
 let wins = 0;
 let losses = 0;
 let crystal1;
@@ -13,26 +13,29 @@ let crystal2;
 let crystal3;
 let crystal4;
 let playerScore = 0;
-reset();
+// reset();
 
+// let targetScore = $("<div>");
+// targetScore.text(random);
+// $("#ali").append(targetScore);
 
 function playerWin() {
     if ((playerScore == random)) {
         wins++;
-        update(1);
-        reset();
+        update();
+        // reset();
         console.log('win');
     }
     if ((playerScore > random)) {
         losses++;
-        update(1);
-        reset();
+        update();
+        // reset();
         console.log('loser');
     }
 }
 
 function update() {
-    const html =
+    let html =
         "<p>Random: " + random + "</p>" +
         "<p>Wins: " + wins + "</p>" +
         "<p>Losses: " + losses + "</p>" +
@@ -43,26 +46,29 @@ function update() {
 }
 
 //resetting the random score and player score while keeping number of wins and losses unchanged
-function reset() {
-    playerScore = 0;
+// function reset() {
+playerScore = 0;
 
-    crystalArray = [crystal1, crystal2, crystal3, crystal4,]
+crystalArray = [crystal1, crystal2, crystal3, crystal4,]
 
-    crystalArray.forEach(function (value, index) {
-        value = Math.floor(Math.random() * (crystalMax - crystalMin)) + crystalMin
-        console.log('Crystal${index+1}: ${value}'); //triggers the function whenever the player clicks that specific button, increments the score and calls the two other functions 
-        document.getElementById('Crystal${index+1}').onclick = function () { //an error here and line 53
-            playerScore += value;
-            update();
-            playerWin();
+for (let i = 0; i < crystalArray.length; i++) {
 
-        }
+    // crystalArray.forEach(function (value, index) {
+    value = Math.floor(Math.random() * (crystalMax - crystalMin)) + crystalMin
+    // console.log('Crystal${index+1}: ${value}'); //triggers the function whenever the player clicks that specific button, increments the score and calls the two other functions 
+    // document.getElementById('Crystal${index+1}').onclick = function () { //an error here and line 53
 
-    })
-
-    random = Math.floor(Math.random() * (+randomMax - +randomMin)) + +randomMin;
+    playerScore += value;
     update();
-    console.log("Random Number Created : " + random);
-    console.log("Computer guess reset", random);
+    playerWin();
 
+    // }
 }
+// })
+
+random = Math.floor(Math.random() * (+randomMax - +randomMin)) + +randomMin;
+update();
+console.log("Random Number Created : " + random);
+console.log("Computer guess reset", random);
+
+// }
